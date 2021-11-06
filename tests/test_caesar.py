245d6a4d2eb78c171f28d468dc31ba9d823dac1c
duajaradat <duajaradat164@gmail.com>
2021-11-06 09:05:39 +0200

    test : test encrypt uppercase text

--- tests/test_caesar.py
+++ tests/test_caesar.py
@@ -6,3 +6,12 @@ def test_encrypt_shift_1():
     expected = "bqqmf"
     assert actual == expected
 
+def test_encrypt_shift_10():
+    actual = encrypt("apple", 10)
+    expected = "kzzvo"
+    assert actual == expected    
+
+def test_uppercase():
+    actual = encrypt("BANANA", 10)
+    expected = "LKXKXK"
+    assert actual == expected
